5fdff8b5743790cc0363d938c82794ef3d94cf3e
Allan Pablo <allanpablo@gmail.com>
2021-02-24 21:57:30 -0300

    init Contexts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
 import "../styles/global.css";
-
+import { ChallengesProvider}  from '../contexts/ChallengesContext'
+import { ChallengeBox } from "../Components/ChallengeBox";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return ( 
+  <ChallengesProvider>
+    <Component {...pageProps} />
+  </ChallengesProvider>
+  )
 }
 
 export default MyApp;
