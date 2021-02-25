62cf0e38115585475e233f238d66147c6d5750e4
Allan Pablo <allanpablo@gmail.com>
2021-02-25 10:48:59 -0300

    Notification Audio

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -45,6 +45,8 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
 
     setActiveChallenge(challenge)
 
+    new Audio ('/notification.mp3').play()
+
     if (Notification.permission ==='granted'){
       new Notification('Novo Desafio 📣', {
        body: `Valendo ${challenge.amount}xp!`})
