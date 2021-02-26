de48daef2bdaa768f5a7ceaa5ebe1415ecad7910
Allan Pablo <allanpablo@gmail.com>
2021-02-26 13:55:53 -0300

    Modal Ok

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,27 +6,54 @@ import { Countdown } from "../Components/Countdown";
 import Head from 'next/head';
 import { ChallengeBox } from "../Components/ChallengeBox";
 import { CountdownProvider } from "../contexts/CountdownContext";
+import {GetServerSideProps} from 'next';
+import { ChallengesProvider } from "../contexts/ChallengesContext";
 
 
-export default function Home() {
+interface HomeProps {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+}
+
+export default function Home(props) {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Início | Move.it</title>
-      </Head>
-      <ExperienceBar />
-      <CountdownProvider>
-        <section>
-          <div>
-            <Profile />
-            <CompletedChallenges />
-            <Countdown />
-          </div>
-          <div>
-            <ChallengeBox />
-          </div>
-        </section>
-        </CountdownProvider>
-    </div>
+    <ChallengesProvider
+    level={props.level}
+    currentExperience={props.currentExperience}
+    challengesCompleted={props.challengesCompleted}>
+    
+        <div className={styles.container}>
+          <Head>
+            <title>Início | Move.it</title>
+          </Head>
+          <ExperienceBar />
+          <CountdownProvider>
+            <section>
+              <div>
+                <Profile />
+                <CompletedChallenges />
+                <Countdown />
+              </div>
+              <div>
+                <ChallengeBox />
+              </div>
+            </section>
+            </CountdownProvider>
+        </div>
+    </ChallengesProvider>
   );
+}
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const {level, currentExperience, challengesCompleted} = ctx.req.cookies;
+
+  return{
+    props:{
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengesCompleted: Number(challengesCompleted)
+
+    }
+  }
+
 }
